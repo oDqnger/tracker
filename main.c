ddb3fc22a031bc374370ec597876a650e5ef36dd
oDqnger <dimanchansilu@gmail.com>
2025-10-27 20:21:10 +0000

    prolly 98% done with this. might need to make some minor improvements and some minor additions. all i did with made it more efficient and less cpu consuming by adding a delays

--- main.c
+++ main.c
@@ -1,35 +1,54 @@
 #include <stdio.h>
 #include <sys/time.h>
-#include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main() {
   struct timeval t1, t2;
   int elapsedTime = 0;
   char s[20];
   int prev;
-  FILE *fptr;
+  FILE *fptr = fopen("./time.txt", "r");
+  if (fptr == NULL) {
+    printf("ERROR: cannot open file\n");
+    return 1;
+  }
+  
+  char data[20];
+  fgets(data, 20, fptr);
+  fclose(fptr);
+
+  FILE *fptr2 = fopen("./time_history.txt", "a");
+  if (fptr2 == NULL) {
+    printf("ERROR: cannot open file\n");
+    return 1;
+  }
+  fprintf(fptr2, data);
+  fclose(fptr2);
+
+  fptr2 = NULL;
+
   gettimeofday(&t1, NULL);
   
   while (1) {
     gettimeofday(&t2, NULL);
-    elapsedTime = (t2.tv_sec - t1.tv_sec);
-    if (elapsedTime % 2 == 0) {
+    elapsedTime = t2.tv_sec - t1.tv_sec;
+    if (elapsedTime % 60 == 0) {
       if (prev != elapsedTime) {
         prev = elapsedTime;
-        printf("hello");
-        fptr = fopen("./config.txt", "w");
+        fptr = fopen("./time.txt", "w");
 
         if (fptr == NULL) {
           printf("ERROR: cannot open file\n");
           return 1;
         }
-        sprintf(s, "%d", elapsedTime);
-        fprintf(fptr, strcat(s, " seconds"));
+        sprintf(s, "%d", (int) elapsedTime / 60);
+        fprintf(fptr, strcat(s, " minutes\n"));
         fclose(fptr);
       }
     }
+    usleep(100000);
   }
   gettimeofday(&t2, NULL);
   elapsedTime = (t2.tv_sec - t1.tv_sec);
