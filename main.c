9ae3d9b127c446c7c79b607693c43061349a28d1
oDqnger <dimanchansilu@gmail.com>
2025-10-27 09:57:07 +0000

    changed some stuff made it slightly better idk idkidkidkidkdikdikdikdidkidkidkidk

--- main.c
+++ main.c
@@ -2,23 +2,23 @@
 #include <sys/time.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
   struct timeval t1, t2;
   int elapsedTime = 0;
-  FILE *fptr;
-  gettimeofday(&t1, NULL);
   char s[20];
   int prev;
-  int counter = 0;
+  FILE *fptr;
+  gettimeofday(&t1, NULL);
+  
   while (1) {
     gettimeofday(&t2, NULL);
     elapsedTime = (t2.tv_sec - t1.tv_sec);
-    if (elapsedTime % 5 == 0) {
+    if (elapsedTime % 2 == 0) {
       if (prev != elapsedTime) {
         prev = elapsedTime;
-        counter++;
-        printf("%d\n", counter);
+        printf("hello");
         fptr = fopen("./config.txt", "w");
 
         if (fptr == NULL) {
@@ -26,12 +26,9 @@ int main() {
           return 1;
         }
         sprintf(s, "%d", elapsedTime);
-        fprintf(fptr, s);
+        fprintf(fptr, strcat(s, " seconds"));
         fclose(fptr);
       }
-    }
-    if (elapsedTime == 10) {
-      break;
     }
   }
   gettimeofday(&t2, NULL);
